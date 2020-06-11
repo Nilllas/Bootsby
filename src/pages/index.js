b67c23666bbc5f0ea7d0c2f1f48d00de3ce33c67
Nilllas <niklasln@gmail.com>
2020-06-11 13:20:35 +0000

    test box

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,20 @@ export default function Home() {
     return (
       <Layout>
  
-        
-      <main>
-         <h1>Hello world!</h1>
+      <h1>Hello world!</h1>
          <h2>Welcome hooman</h2>
-      </main>
-      
+         <p>testing this weird text</p>
+      <p></p>
+      <div>
+        <ul className="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+      <li><a tabindex="-1" href="/">Action</a></li>
+      <li><a tabindex="-1" href="/">Another action</a></li>
+      <li><a tabindex="-1" href="/">Something else here</a></li>
+      <li className="divider"></li>
+      <li><a tabindex="-1" href="/">Separated link</a></li>
+    </ul>
+</div>
+
       </Layout>
 
     );
